0d2817b6a94e9e4317e6b3d010297554880e2fa4
hasan2170 <arabi.hasan@northsouth.edu>
2019-03-20 23:24:46 +0600

    Empty class with only function signatures created.

---
+++ North South University/Spring2019/Assignment1/src/GenericStack.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sakib
+ *
+ */
+
+public class GenericStack {
+
+    public GenericStack() {
+       
+    }
+
+    public void push(Object x) {
+
+    }
+    
+    public Object pop() {
+		return null;
+    }
+    
+    public boolean isEmpty() {
+		return false;
+    }
+}
